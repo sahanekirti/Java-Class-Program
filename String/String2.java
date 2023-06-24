38c9dc9b7e7e3859f4bb6a7ab03186eb21a86864
Sahane_Kirti <kirtisahane2001@gmail.com>
2023-06-24 21:48:42 +0530

    initial commit

---
+++ String/String2.java
@@ -0,0 +1,23 @@
+package string2;
+
+import java.util.Scanner;
+
+public class String2 {
+
+		public static void main(String[] args) {
+			Scanner sc=new Scanner(System.in);
+			System.out.println("enter a data");
+			String s=sc.nextLine();
+			
+			
+			for(int i=s.length()-1; i>=0;i--)
+			{
+				System.out.print(s.charAt(i));
+			}
+			
+		}
+
+	}
+
+
+
